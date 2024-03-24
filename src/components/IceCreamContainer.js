6df9a270bcddc4e2d55cdf5e3d1ed65d1af977bb
sumandul <dulalsuman853@gmail.com>
2024-03-24 22:36:57 +0545

    feat: Implemented logic according to the input provided

--- src/components/IceCreamContainer.js
+++ src/components/IceCreamContainer.js
@@ -1,8 +1,9 @@
-import React from "react";
+import React, { useState } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { buyIceCream } from "../redux";
 
 function IcecreamContainer() {
+  const [number, setNumber] = useState(1);
   // const numOfIceCream = useSelector((state) => state.iceCream.numOfIceCream);
   //! more optimize way to use useSelector
   const numOfIceCream = useSelector(({ iceCream }) => iceCream.numOfIceCream);
@@ -10,7 +11,14 @@ function IcecreamContainer() {
   return (
     <div>
       <h2>Number of IceCream - {numOfIceCream}</h2>
-      <button onClick={() => dispatch(buyIceCream())}>Buy iceCream</button>
+      <input
+        type="text"
+        value={number}
+        onChange={(e) => setNumber(e.target.value)}
+      />
+      <button onClick={() => dispatch(buyIceCream(number))}>
+        Buy {number} iceCream
+      </button>
     </div>
   );
 }
--- src/redux/actions/iceCreamAction.js
+++ src/redux/actions/iceCreamAction.js
@@ -2,8 +2,9 @@ import { types } from "./types";
 
 const { BUY_ICECREAM } = types;
 
-export const buyIceCream = () => {
+export const buyIceCream = (number = 1) => {
   return {
     type: BUY_ICECREAM,
+    payload: number,
   };
 };
